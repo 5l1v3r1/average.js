4f032ad17b07027184bc64af73044a345d81a962
Alex Nichol <unixpickle@gmail.com>
2015-05-03 12:09:52 -0400

    implemented _removeOldestTime for CenterAverage

--- src/center_average.js
+++ src/center_average.js
@@ -16,7 +16,7 @@ function CenterAverage(size, numRemove) {
 }
 
 // average returns the current average or NaN if there were not enough actual
-// actual values.
+// values.
 CenterAverage.prototype.average = function() {
   if (Math.max(this._posInfCount, this._negInfCount) > this._numRemove ||
       this._sortedTimes.count() < this._size) {
@@ -29,8 +29,39 @@ CenterAverage.prototype.average = function() {
 // pushTime adds the next time to the rolling average and removes the very
 // first time.
 CenterAverage.prototype.pushTime = function(time) {
-  // TODO: this.
+  this._removeOldestTime();
+
+  // TODO: add the new time here.
+
   throw new Error('not yet implemented');
+};
+
+CenterAverage.prototype._removeOldestTime = function() {
+  var oldTime = this._chronologicalTimes.shift();
+  if (oldTime === Infinity) {
+    --this._posInfCount;
+  } else if (oldTime === -Infinity) {
+    --this._negInfCount;
+  }
+
+  // this._average may have to be updated after removing the time, since
+  // |LLL|MMMMMMMM|HHH| might have become |LLM|MMMMMMMH|HH | or
+  // |LLL|MMMMMMMH|HH |. If it became |LLL|MMMMMMMM|HH |, nothing changed in the
+  // middle.
+
+  var removedIndex = this._sortedTimes.remove(oldTime);
+  if (removedIndex < this._numRemove) {
+    var newLowIndex = this._numRemove - 1;
+    if (this._sortedTimes.count() > newLowIndex) {
+      this._average.remove(this._sortedTimes.get(newLowIndex));
+    }
+  }
+  if (removeIndex < this._size - this._numRemove) {
+    var newMiddleIndex = this._size - this._numRemove - 1;
+    if (this._sortedTimes.count() > newMiddleIndex) {
+      this._average.add(this._sortedTimes.get(newMiddleIndex));
+    }
+  }
 };
 
 exports.CenterAverage = CenterAverage;
